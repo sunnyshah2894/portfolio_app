9bd38df39721e90a957e06ede693d3dedcf1e5a7
unknown <Administrator@celab2-3a.comp1-domain.spit.ac.in>
2012-09-16 14:32:51 +0530

    dropbox

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioApp::Application.config.secret_token = 'fd1ee83a83ba9c41be1e515fe5a028ba06e964e951a0c492caa2dad59f53ce594ce097098de7713874ca71534bff895465abade63dbfb578677c8ea454eb7e11'
