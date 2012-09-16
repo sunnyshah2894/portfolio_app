3aa7652cb7556ae75c4d4a9a5512e354612153bb
unknown <Administrator@celab2-3a.comp1-domain.spit.ac.in>
2012-09-16 14:59:39 +0530

    errors added\

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,14 @@
 class Company < ActiveRecord::Base
+  validate :symbol_length_should_be_less
+  def symbol_length_should_be_less
+    if symbol.length <3 or symbol.length>4
+      errors.add :symbol,'should be either 3 or 4 lettered'
+    end
+  end
+  validate :should_be_positive
+  def should_be_positive
+    if price <0
+      errors.add :price,'Should Be Positive'
+    end
+    end
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,11 @@
 class Investment < ActiveRecord::Base
+validate :should_be_positive
+  def should_be_positive
+    if quantity <0
+      errors.add :quantity,'Should Be Positive'
+    end
+    if cost<0
+      errors.add :cost,'Should Be Positive'
+    end
+  end
 end
